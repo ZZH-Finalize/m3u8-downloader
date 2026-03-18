4907a1113a3ffc5224471cf5ae8c876b6f5fcfbd
ZZH <1462207500@qq.com>
2026-03-18 15:21:19 +0800

    fix potential issues
    Signed-off-by: ZZH <1462207500@qq.com>

--- backend_new/downloader.py
+++ backend_new/downloader.py
@@ -5,7 +5,7 @@ from models import TaskStatus, SegmentInfo
 from task import DownloadTask
 from logger import get_logger
 from config import server_config as config
-from urllib.parse import urlparse, unquote
+from urllib.parse import urlparse, unquote, urljoin
 
 logger = get_logger(__name__)
 
@@ -33,15 +33,20 @@ async def download_segment(session: aiohttp.ClientSession, task: DownloadTask):
             break
 
         url_fn = os.path.basename(unquote(urlparse(segment.url).path))
-        abs_url = task.metadata.base_url + segment.url
+        abs_url = urljoin(task.metadata.base_url, segment.url)
 
         logger.info(f'[{task.id}] 下载分片[{segment.id + 1}/{task.metadata.segments_num}]')
 
-        async with session.get(abs_url) as response:
-            response.raise_for_status()
-            content = await response.read()
-            await task.save_segment(url_fn, segment.id, content)
-            logger.debug(f'[{task.id}] 保存分片文件: {url_fn}')
+        for i in range(MAX_RETRY):
+            try:
+                async with session.get(abs_url) as response:
+                    response.raise_for_status()
+                    content = await response.read()
+                    await task.save_segment(url_fn, segment.id, content)
+                    logger.debug(f'[{task.id}] 保存分片文件: {url_fn}')
+                    break
+            except Exception:
+                logger.error(f'[{task.id}] 分片下载失败, 重试[{i + 1}/{MAX_RETRY}]')
 
         task.url_queue.task_done()
 
@@ -62,28 +67,32 @@ async def download_segments(task: DownloadTask):
     timeout = aiohttp.ClientTimeout(total=10)
     connector = aiohttp.TCPConnector(limit=task.max_threads)
 
-    async with aiohttp.ClientSession(headers=HEADERS,
-                                    timeout=timeout,
-                                    connector=connector
-    ) as session:
-        for i in range(MAX_ROUND):
-            logger.info(f'[{task.id}] ==========第 {i + 1} 轮下载==========')
-            failed_bits = task.metadata.downloaded_mask.search(0)
-            failed_segments = [SegmentInfo(id=j, url=task.metadata.segments[j]) 
-                        for j in failed_bits]
+    try:
+        async with aiohttp.ClientSession(headers=HEADERS,
+                                        timeout=timeout,
+                                        connector=connector
+        ) as session:
+            for i in range(MAX_ROUND):
+                logger.info(f'[{task.id}] ==========第 {i + 1} 轮下载==========')
+                failed_bits = task.metadata.downloaded_mask.search(0)
+                failed_segments = [SegmentInfo(id=j, url=task.metadata.segments[j])
+                            for j in failed_bits]
 
-            if not failed_segments:
-                logger.info(f'[{task.id}] 所有分片均已下载完成')
-                break
+                if not failed_segments:
+                    logger.info(f'[{task.id}] 所有分片均已下载完成')
+                    break
 
-            logger.info(f'[{task.id}] 本轮需要下载 {len(failed_segments)} 个分片')
+                logger.info(f'[{task.id}] 本轮需要下载 {len(failed_segments)} 个分片')
 
-            for segment in failed_segments:
-                task.url_queue.put_nowait(segment)
+                for segment in failed_segments:
+                    task.url_queue.put_nowait(segment)
 
-            task.url_queue.put_nowait(None)
+                task.url_queue.put_nowait(None)
 
-            await download_round(session, task)
+                await download_round(session, task)
+    except Exception as e:
+        logger.error(f'[{task.id}] 下载过程中出现异常：{e}')
+        raise
 
 
 
